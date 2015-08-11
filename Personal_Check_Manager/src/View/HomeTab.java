9ef9cff82199f85548e82543eb85328df0f7614e
Truong <Truong@Truong_PC.home>
2015-08-10 22:56:28 -0400

    Added 2 Tab classes. Set up Grid to display result from database.

---
+++ Personal_Check_Manager/src/View/HomeTab.java
@@ -0,0 +1,35 @@
+package View;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class HomeTab extends JPanel {
+
+	private static JTable table;
+	private static JScrollPane scrollPane;
+	private String[] columnNames = {"ID", "Date", "Amount", "State Tax", "Federal Tax", "Pension", "Medical"};
+	
+	public HomeTab() {
+	
+		initializeComponents(); 
+		createPanel();
+	}
+	
+	public void initializeComponents() {
+		
+		String [][] sample = new String[100][7];
+		
+		table = new JTable(sample, columnNames);
+		scrollPane = new JScrollPane(table);
+		scrollPane.setPreferredSize(new Dimension(600,535));
+	}
+	
+	public void createPanel() {
+		
+		add(scrollPane);
+		
+	}
+}
--- Personal_Check_Manager/src/View/Main_GUI.java
+++ Personal_Check_Manager/src/View/Main_GUI.java
@@ -12,24 +12,26 @@ public class Main_GUI {
 	
 	public static void main(String[] args) {
 		
+		initializeComponents();
 		createMainFrame();
 	}
 	
-	static void createMainFrame() {
+	static void initializeComponents() {
 		
 		frame = new JFrame("Personal Check Manager");
-		frame.setLocationRelativeTo(null);
-		frame.setMinimumSize(new Dimension(700,700));
+		panel = new JPanel[2];
+		panel[0] = new HomeTab();
+		panel[1] = new AddTab();
+		
+	}
+	static void createMainFrame() {
+		
+		frame.setMinimumSize(new Dimension(1100,600));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		panel = new JPanel[3];
-		for(int i = 0; i < 3; i++) {
-			panel[i] = new JPanel();
-		}
 		tabbedPane = new JTabbedPane();
-		tabbedPane.addTab("Add", panel[0]);
-		tabbedPane.addTab("Delete", panel[1]);
-		tabbedPane.addTab("Search", panel[2]);
+		tabbedPane.addTab("<html><body marginwidth = 35 marginheight = 5>Home</body></html>", panel[0]);
+		tabbedPane.addTab("<html><body marginwidth = 35 marginheight = 5>Add</body></html>", panel[1]);
 		frame.add(tabbedPane);
 	}
 }
